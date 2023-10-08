f68d0ac73b924bf27caa7d985bc41cb002c2fd00
Grecia Barandiaran <greciabarandiaran@gmail.com>
2023-10-08 11:16:20 +0000

    class 1

--- 01_02b/index.php
+++ 01_02b/index.php
@@ -1,11 +1,22 @@
 <?php
 // Use the Console / Terminal for this exercise! 
-$number = 27;
-$guess = readline( 'Guess between 1 and 30: ');
-if ( $number == $guess ) {
-  echo "Yes! You guessed correctly. \n\n";
-} else {
-  echo "Alas. Your guess is incorrect. \n\n";
+$number = rand(1, 30);
+$guess = null;
+
+while ($guess != $number) {
+  $guess = readline('Guess between 1 and 30: ');
+  if ($number == $guess) {
+    echo "Yes! You guessed correctly. \n\n";
+  } else {
+    echo "Alas. Your guess is incorrect. \n\n";
+  }
 }
+// $guess = readline( 'Guess between 1 and 30: ');
+// if ( $number == $guess ) {
+//   echo "Yes! You guessed correctly. \n\n";
+// } else {
+//   echo "Alas. Your guess is incorrect. \n\n";
+// }
 
 die();
+//only works on command line, no browser btw
--- 01_03b/index.php
+++ 01_03b/index.php
@@ -13,11 +13,21 @@
 
 <?php
 
-function flip() {
-	return ( 0 == rand(0,1) ) ? 'H' : 'T';
+function flip()
+{
+	return (0 == rand(0, 1)) ? 'H' : 'T';
 }
 
+$H = 0;
+$T = 0;
+
+for ($i = 0; $i < 1000; $i++) {
+	$flip = flip();
+	${$flip}++;
+}
+echo '<p> Heads was flipped' . $H / 10 . '% of the time. Tails was flipped ' . $T / 10 . '%.'
 ?>
+
 
 <div class="coin">
 	<?php echo flip(); ?>
